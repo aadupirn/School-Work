225f51deb9ce483297568b644252e020551794dd
Aadu <aap75@pitt.edu>
2016-02-02 16:30:07 -0500

    notes

---
+++ ID3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+//COE449 Blackjack part of Programming assignment 1
+//Aadu Pirn, aap75, 3845682
+
+//Function prototypeing
+int getRand();
+
+int main()
+{
