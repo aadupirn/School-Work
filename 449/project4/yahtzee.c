c651ff94ead1704b71ca217065a46db99dcfd9b7
Aadu <pirnaadu@gmail.com>
2016-04-10 20:08:27 -0400

    yahtzee works

--- 449/project4/yahtzee.c
+++ 449/project4/yahtzee.c
@@ -1,13 +1,14 @@
 //Aadu Pirn
 //aap75
 //Project 3 cs449
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
 void printDice(int roll[]);
-int cmpfunc (const void * a, const void * b);
+int compfunc (const void * a, const void * b);
 
 int main()
 {
@@ -33,8 +34,7 @@ Large Straight......50\n\
 Yahtzee.............50\n\
 Chance..............Sum of all 5 dice\n\
 ------------------------------\n\
-A score of more than 63 points in the upper section will result in a ponus 35 points.\n\n\
-Here is your first roll:\n");
+A score of more than 63 points in the upper section will result in a ponus 35 points.\n\n");
 	int points[13];
 	int i;
 	for(i = 0; i < 13; i++)
@@ -43,6 +43,7 @@ Here is your first roll:\n");
 	}
 	while(roundCount < 13)
 	{
+		printf("Here is your roll:\n");
 		int roll[5];
 		char rerollString[100];
 		roll[0] = (rand()%6) + 1;
@@ -53,31 +54,29 @@ Here is your first roll:\n");
 		rollCount++;
 		while(rollCount < 3)
 		{
+			printf("\n");
 			printDice(roll);
-			printf("\nPlease enter the number of the die/dice you want to reroll (Enter zero to skip rerolls):");
+			printf("\nPlease enter the number of dice you want to reroll: ");
+			
 			int rerolls[5];
 			for(i = 0; i < 5; i++)
 			{
 				rerolls[i] = -1;
 			}
-			int num;
-			char buffer[1000];
-			fgets(buffer, sizeof buffer, stdin);
-			const char *p = buffer;
-			int n;
-			int count = 0;
-			while (sscanf(p, "%d%n", &num, &n) == 1)  //getting numbers that are rerolled
-			{
-				p += n;
-				rerolls[count] = num;
-				count++;
-			} 
+			int numR;
+			scanf("%d", &numR);
+			
 			
 			if(rerolls[0] == 0)
 			{
 					break;
 			}
-			for(i = 0; i < 5; i++)
+			for(i = 0; i < numR; i++)
+			{
+				printf("\nEnter the index of a die you want to reroll: ");
+				scanf("%d", &rerolls[i]);
+			}
+			for(i = 0; i < numR; i++)
 			{
 				if(rerolls[i] != -1)
 				{
@@ -89,41 +88,275 @@ Here is your first roll:\n");
 				}
 			}
 			rollCount++;
-			printf("%d", rollCount);
 		}
+		printf("\nYour final roll is: ");
+		printDice(roll);
 		rollCount = 0;
-		qsort(roll, 5, sizeof(int), cmpfunc);
+		qsort(roll, 5, sizeof(int), compfunc);
 		int threeKind = 0;
 		int fourKind = 0;
 		int yahtzee = 0;
 		int smallS = 0;
 		int largeS = 0;
-		int kindCounter = 1;
+		int fullH = 0;
+		int sameCounter = 1;
 		int straightCounter = 1;
+		int bestSameCard = 1;
+		int bestStraight = 1;
+		int sameCard = 0;
 		for(i = 1; i < 5; i++)
 		{
 			if(roll[i] == roll[i-1])
 			{
-				kindCounter++;
+				sameCounter++;
+				if(sameCounter > bestSameCard)
+				{
+					bestSameCard = sameCounter;
+					sameCard = roll[i];
+				}
 			}
-			if(roll[i] = (roll[i-1] + 1))
+			else
+			{
+				sameCounter = 1;
+			}
+			if(roll[i] == (roll[i-1] + 1))
 			{
 				straightCounter++;
+				if(straightCounter > bestStraight)
+				{
+					bestStraight = straightCounter;
+				}
+			}
+			else
+			{
+				straightCounter = 1;
 			}
 		}
-		
+		if(((roll[0] == roll[1]) && (roll[2] == roll[3]) && (roll[3] == roll[4])) ||
+		((roll[0] == roll[1]) && (roll[1] == roll[2]) && (roll[3] == roll[4]))
+		)
+		{
+			fullH = 1;
+		}
+		printf("\nSorted it looks like this: ");
+		printDice(roll);
+		if(sameCounter>2)
+		{
+			threeKind = 1;
+			if(sameCounter>3)
+			{
+				fourKind = 1;
+				if(sameCounter>4)
+				{
+					yahtzee = 1;
+				}
+			}
+		}
+		if(straightCounter>3)
+		{
+			smallS = 1;
+			if(straightCounter>4)
+			{
+				largeS = 1;
+			}
+		}
+		printf("\n\nHere is your board so far. You can apply your dice to any section without points.\n\
+Applying your dice to a category where it would score 0 will score 0 for that category.\n\
+If the current point total for a section is -1 then you have not scored there yet and still need to fill it.\n\
+--------UPPER SECTION--------\n\
+1) Ones................... %d\n\
+2) Twos................... %d\n\
+3) Threes................. %d\n\
+4) Fours.................. %d\n\
+5) Fives.................. %d\n\
+6) Sixes.................. %d\n\
+--------LOWER SECTION--------\n\
+7) Three of a Kind........ %d\n\
+8) Four of a Kind......... %d\n\
+9) Full House............. %d\n\
+10) Small Straight........ %d\n\
+11) Large Straight........ %d\n\
+12) Yahtzee............... %d\n\
+13) Chance................ %d\n\
+------------------------------\n\
+Enter a number 1-13 to choose where to apply this roll: \n", points[0], points[1], points[2], points[3], points[4], points[5], points[6], points[7], points[8], points[9], points[10], points[11], points[12]);
+		int pointApplyChoice;
+		scanf("%d", &pointApplyChoice);
+		int pointSum = 0;
+		switch(pointApplyChoice)
+		{
+			case(1):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 1)
+					{
+						pointSum = pointSum + 1;
+					}
+				}
+				points[0] = pointSum;
+				break;
+			case(2):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 2)
+					{
+						pointSum = pointSum + 2;
+					}
+				}
+				points[1] = pointSum;
+				break;
+			case(3):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 3)
+					{
+						pointSum = pointSum + 3;
+					}
+				}
+				points[2] = pointSum;
+				break;
+			case(4):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 4)
+					{
+						pointSum = pointSum + 4;
+					}
+				}
+				points[3] = pointSum;
+				break;
+			case(5):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 5)
+					{
+						pointSum = pointSum + 5;
+					}
+				}
+				points[4] = pointSum;
+				break;
+			case(6):
+				for(i = 0; i < 5; i++)
+				{
+					if(roll[i] == 6)
+					{
+						pointSum = pointSum + 6;
+					}
+				}
+				points[5] = pointSum;
+				break;
+			case(7):
+				for(i = 0; i < 5; i++)
+				{
+					pointSum = pointSum + roll[i];	
+				}
+				points[6] = pointSum;
+				break;
+			case(8):
+				for(i = 0; i < 5; i++)
+				{
+					pointSum = pointSum + roll[i];	
+				}
+				points[7] = pointSum;
+				break;
+			case(9):
+				if(fullH)
+				{
+					points[8] = 25;
+				}
+				else
+				{
+					points[8] = 0;
+				}
+				break;
+			case(10):
+				if(smallS)
+				{
+					points[9] = 30;
+				}
+				else
+				{
+					points[9] = 00;
+				}
+				break;
+			case(11):
+				if(largeS)
+				{
+					points[10] = 40;
+				}
+				else
+				{
+					points[10] = 0;
+				}
+				break;
+			case(12):
+				if(yahtzee)
+				{
+					points[11] = 50;
+				}
+				else
+				{
+					points[11] = 0;
+				}
+				break;
+			case(13):
+				for(i = 0; i < 5; i++)
+				{
+					pointSum = pointSum + roll[i];	
+				}
+				points[12] = pointSum;
+				break;
+		}		
 		roundCount++;
 	}
+	int upperSum = 0;
+	int totalSum = 0;
+	int bonus = 0;
+	for(i = 0; i < 6; i++)
+	{
+		upperSum = upperSum + points[i];
+	}
+	if(upperSum > 62)
+	{
+		bonus = 35;
+		totalSum = upperSum + 35;
+	}
+	else
+	{
+		totalSum = upperSum;
+	}
+	for(i = 0; i < 13; i++)
+	{
+		totalSum = totalSum + points[i];
+	}
+	
+	printf("\n\nHere are your final scores!!\n\
+--------UPPER SECTION--------\n\
+1) Ones................... %d\n\
+2) Twos................... %d\n\
+3) Threes................. %d\n\
+4) Fours.................. %d\n\
+5) Fives.................. %d\n\
+6) Sixes.................. %d\n\
+--------LOWER SECTION--------\n\
+7) Three of a Kind........ %d\n\
+8) Four of a Kind......... %d\n\
+9) Full House............. %d\n\
+10) Small Straight........ %d\n\
+11) Large Straight........ %d\n\
+12) Yahtzee............... %d\n\
+13) Chance................ %d\n\
+------------------------------\n\
+Bonus: %d\n\
+Total: %d\n", points[0], points[1], points[2], points[3], points[4], points[5], points[6], points[7], points[8], points[9], points[10], points[11], points[12], bonus, totalSum);
+	
 	
 	
 
 	return 0;
-	
-   
-
 }
 
-int cmpfunc (const void * a, const void * b)
+int compfunc (const void * a, const void * b)
 {
    return ( *(int*)a - *(int*)b );
 }
