12a3276f9bc3f77af1e65a8a4cd1a7fdcb5dbf81
Aadu <aap75@pitt.edu>
2016-04-23 13:08:52 -0400

    update

--- 449/project5/myshell.c
+++ 449/project5/myshell.c
@@ -11,83 +11,85 @@
 #include <string.h>
 
 //constants for parsing strings.
-const char LEFT[2] = "<";
-const char RIGHT[2] = ">";
-const char SPACE[2] = " ";
-const char LPAR[2] = "(";
-const char RPAR[2] = ")";
-const char ORCH[2] = "|";
-const char ANDCH[2] = "&";
-const char DASH[2] = "-";
+const char delims[100] = "\t \n()<>|&;";
 
 char pwd[1024]; //string to get present working directory. Going to start at the location where this executable resides.
 char command[1024]; //string to stare desired command in.
 	
 char *token;
+
+int execute(char **arg)
+{
+	pid_t id;
+	int status;
+	id = fork();
 	
+	if(id < 0)
+	{
+		printf("ERROR: forking process failure\n");
+	}
+	else if(id == 0)
+	{
+		if(execvp(*arg, arg) < 0)
+		{
+			printf("ERROR: falure to execute command\n");
+		}
+	}
+	else
+	{
+		while(wait(&status) != id)
+		{}
+	}
+}
 
 int main()
 {
 	//Print greeting message
 	printf("\nInitializing your very own Unix based Shell...\nYou honestly should probably use whatever shell you started this from... but whatever.\nDeveloped by Aadu Pirn\nDeveloped for CS0449 at The University of Pittsburgh\n\n");
-
+	char *arg[64];
 	
 	
 	int run = 1;
 	while(run)
 	{
 		getcwd(pwd, sizeof(pwd));
-		printf("SHELL:%s$ ", pwd);
+		printf("SHELL:%s $ ", pwd);
 		fgets(command, 1024, stdin);
-		token = strtok(command, SPACE);
-		printf("\n\nCommand: %s\n\n", command);
+		token = strtok(command, delims);
+		
 		if(strcmp(token, "cd") == 0)
 		{
-			
+			token = strtok(NULL, delims);
+			if(chdir(token)<0)
+			{
+				printf("ERROR: invalid directory\n");
+			}
 		}
 		else if(strcmp(token, "exit") == 0)
 		{
-			
+			run = 0;
 		}
-		else if(strcmp(token, "ls") == 0)
+		else
 		{
-			
+			if(strcmp(token, "time") == 0)
+			{
+				
+			}
+			int counter = 0;
+			while(token != NULL)
+			{
+				arg[counter] = token;
+				counter++;
+				token = strtok(NULL, delims);
+			}
+			arg[counter] = NULL;
+			execute(arg);
 		}
-		else if(strcmp(token, "pico") == 0)
-		{
-			
-		}
-		else if(strcmp(token, "pwd") == 0)
-		{
-			printf("You asked for the present working directory!!");
-			token = strtok(NULL, SPACE);
-			printf("the argument of the command was: %s", token);
-		}
-		else if(strcmp(token, "ls") == 0)
-		{
-			
-		}
-		else if(strcmp(token, "wc") == 0)
-		{
-			
-		}
-		else if(strcmp(token, "ps") == 0)
-		{
-			
-		}
-		else if(strcmp(token, "time") == 0)
-		{
-			
-		}
-		else if(strcmp(token, "cd") == 0)
-		{
-			
-		}
-		run = 0;
 	}
 	
 	
 	
 	printf("\n\nExiting this awesome shell. Good luck in the world without me.\n\n");
-	return 0;
+	exit(0);
+	return(0);
 }
